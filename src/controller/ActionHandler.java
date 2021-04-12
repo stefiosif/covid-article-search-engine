eb1e3b4b076fa5fcec9cf5c28454af28cf724da0
stefiosif <stef8iosif@gmail.com>
2021-04-12 18:25:14 +0300

    Setup JavaFX, SceneBuilder and Lucene

---
+++ src/controller/ActionHandler.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+public class ActionHandler {
+
+    @FXML
+    private void searchDocuments(){
+        System.out.println("searching...");
+    }
+}
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,19 +1,24 @@
-package sample;
+package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Button button;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Parent root = FXMLLoader.load(getClass().getResource("app-gui.fxml"));
+        primaryStage.setTitle("Covid19 Search Engine");
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
+
     }
 
 
